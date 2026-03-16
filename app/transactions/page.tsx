ed96eed240ebfad565e6aae22929e7d9c1358182
Nilson <netohjunior216@gmail.com>
2026-03-16 07:44:38 -0300

    refactor: update TransactionsPage layout and remove transaction list

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,12 +1,19 @@
+import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
+import {  ArrowDownUpIcon,  } from "lucide-react";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
   return (
     <div className="space-y-6 p-6">
-      {transactions.map((transaction) => (
-        <div key={transaction.id}>{transaction.name}</div>
-      ))}
+      {/*TITULO E BOTÃO*/}
+      <div className="flex w-full justify-between items-center">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
     </div>
   );
 };
